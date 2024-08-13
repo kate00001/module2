50eb593b1a84af62697f6525a383eb25fd384100
Kate <166659076+kate00001@users.noreply.github.com>
2024-08-14 00:52:51 +0300

    Add files via upload

---
+++ module_2_4.py
@@ -0,0 +1,12 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+primes = []
+not_primes = []
+for i in numbers:
+    if i > 1:
+        for d in range(2, i):
+            if (i % d) == 0:
+                not_primes.append(i)
+                break
+        else: primes.append(i)
+print(primes)
+print(not_primes)
